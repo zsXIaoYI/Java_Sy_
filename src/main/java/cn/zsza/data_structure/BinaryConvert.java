db69bba60cf5c5baebd11ba73d9012defaf31610
zhangsong <zsza5466@sina.com>
2018-07-07 15:40:24 +0800

    添加进制转换

---
+++ src/main/java/cn/zsza/data_structure/BinaryConvert.java
@@ -0,0 +1,21 @@
+package cn.zsza.data_structure;
+
+import org.junit.Test;
+/**
+ * Created by ZhangSong on 2017/9/23.
+ * 17:30
+ * 进制转换
+ */
+public class BinaryConvert {
+    // 十进制转换成二级制
+    @Test
+    public void testTen_Two(){
+        int num = 10;
+        StringBuffer sb = new StringBuffer();
+        while(num>0){
+            sb.append(num % 2);
+            num = num/2;
+        }
+        System.out.println("十进制数转换二进制位："+sb.reverse());
+    }
+}
