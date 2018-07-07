c0bac127e883b22151f75c25aef714b9c8b57c9e
zhangsong <zsza5466@sina.com>
2018-07-07 18:16:49 +0800

    新增适配器模式

---
+++ src/main/java/cn/zsza/design_pattern/adapter/Target.java
@@ -0,0 +1,21 @@
+package cn.zsza.design_pattern.adapter;
+
+/**
+ * Created by zs on 2017/8/18.
+ * 9:58
+ * 目标(Target)角色
+ */
+public interface Target {
+
+
+    /**
+     * 这是源类Adaptee也有的方法
+     */
+    public void sampleOperation1();
+
+
+    /**
+     * 这是源类Adaptee没有的方法
+     */
+    public void sampleOperation2();
+}
