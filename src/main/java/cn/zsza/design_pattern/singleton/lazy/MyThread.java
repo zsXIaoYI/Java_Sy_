b7d0cb9bd73aa3b91d48b4c458232d400862814a
zhangsong <zsza5466@sina.com>
2018-07-07 14:53:31 +0800

    添加单例设计模式

---
+++ src/main/java/cn/zsza/design_pattern/singleton/lazy/MyThread.java
@@ -0,0 +1,14 @@
+package cn.zsza.design_pattern.singleton.lazy;
+
+/**
+ * Created by zs on 2017/7/25.
+ * 16:38
+ */
+public class MyThread extends Thread {
+
+    @Override
+    public void run() {
+
+        System.out.println(MyObject.getInstance().hashCode());
+    }
+}
