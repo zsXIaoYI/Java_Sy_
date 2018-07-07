59925dcd7a3e316e8df544f8a5f9d2a006f7d7d5
zhangsong <zsza5466@sina.com>
2018-07-07 15:12:47 +0800

    新增Node实体

---
+++ src/main/java/cn/zsza/common/entity/Node.java
@@ -0,0 +1,21 @@
+package cn.zsza.common.entity;
+
+import lombok.Data;
+/**
+ * Created by ZhangSong on 2017/9/19.
+ * 11:34
+ */
+@Data
+public class Node<T>{
+    private T data;
+    private Node next;
+
+    public Node() {
+    }
+
+    public Node(T obj) {
+        this.data = obj;
+    }
+
+
+}
